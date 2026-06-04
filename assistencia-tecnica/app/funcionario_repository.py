8d788098e08923c97b5d4772a5c814e79c77f8a6
Mariana <92758993+mariana097@users.noreply.github.com>
2026-06-04 17:41:24 -0300

    Delete assistencia-tecnica/app/funcionario_repository.py

--- assistencia-tecnica/app/funcionario_repository.py
+++
@@ -1,7 +0,0 @@
-from app.models.funcionario import Funcionario
-from app.repositories.base_repository import BaseRepository
-
-
-class FuncionarioRepository(BaseRepository):
-    def __init__(self, session):
-        super().__init__(session, Funcionario)
