87db31fbc6b170f124c736e5ebe41ce2e5d60d57
Mariana <92758993+mariana097@users.noreply.github.com>
2026-06-04 17:41:35 -0300

    Delete assistencia-tecnica/app/cliente_repository.py

--- assistencia-tecnica/app/cliente_repository.py
+++
@@ -1,7 +0,0 @@
-from app.models.cliente import Cliente
-from app.repositories.base_repository import BaseRepository
-
-
-class ClienteRepository(BaseRepository):
-    def __init__(self, session):
-        super().__init__(session, Cliente)
